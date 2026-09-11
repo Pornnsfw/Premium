d3db4bac27190a9901e0d16726d30fb37d14e531
AX <pornnsfwsafe@gmail.com>
2026-09-11 04:10:53 -0700

    Create LayByLose.lua

---
+++ Strats/LayByLose.lua
@@ -0,0 +1,105 @@
+TDS:Loadout("Soldier", "Assassin", "None", "None", "None")
+TDS:Mode("Molten")
+TDS:GameInfo("Lay By", {})
+
+TDS:Place("Soldier", -2.623168468475342, 262.99998474121094, 271.58221435546875)
+TDS:Upgrade(1)
+TDS:Ready()
+
+-- [ Wave 2 ] --
+TDS:Place("Soldier", -2.623168468475342, 262.99998474121094, 271.58221435546875)
+TDS:Upgrade(2)
+
+-- [ Wave 3 ] --
+TDS:Place("Soldier", -3.0838632583618164, 262.99998474121094, 271.7314147949219)
+TDS:Upgrade(3)
+
+-- [ Wave 4 ] --
+TDS:Place("Soldier", -3.1164636611938477, 262.99998474121094, 272.2812805175781)
+TDS:Upgrade(4)
+
+-- [ Wave 5 ] --
+TDS:Place("Assassin", -2.550779342651367, 262.99998474121094, 282.4583435058594)
+TDS:Upgrade(5)
+TDS:Place("Assassin", 2.668839454650879, 262.99998474121094, 282.76556396484375)
+
+-- [ Wave 6 ] --
+TDS:Upgrade(6)
+TDS:Place("Assassin", 2.557888984680176, 262.99998474121094, 280.81756591796875)
+TDS:Place("Assassin", -2.6355390548706055, 262.99998474121094, 280.7108154296875)
+TDS:Upgrade(8)
+
+-- [ Wave 7 ] --
+TDS:Place("Assassin", 2.508328437805176, 262.99998474121094, 279.5216369628906)
+TDS:Place("Assassin", -2.72670841217041, 262.99998474121094, 278.9090881347656)
+
+-- [ Wave 8 ] --
+TDS:Place("Assassin", 2.5100202560424805, 262.99998474121094, 278.420654296875)
+TDS:Place("Assassin", -2.677440643310547, 262.99998474121094, 276.8633728027344)
+TDS:Upgrade(12)
+TDS:Upgrade(12)
+
+-- [ Wave 9 ] --
+TDS:Upgrade(10)
+TDS:Upgrade(10)
+TDS:Upgrade(9)
+TDS:Upgrade(9)
+
+-- [ Wave 10 ] --
+TDS:Upgrade(9)
+
+-- [ Wave 11 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+
+-- [ Wave 12 ] --
+TDS:Upgrade(3)
+TDS:Upgrade(4)
+TDS:Upgrade(10)
+
+-- [ Wave 13 ] --
+TDS:Upgrade(8)
+TDS:Upgrade(5)
+TDS:Upgrade(5)
+
+-- [ Wave 14 ] --
+TDS:Upgrade(6)
+TDS:Upgrade(4)
+
+-- [ Wave 15 ] --
+TDS:Upgrade(1)
+TDS:Upgrade(2)
+
+-- [ Wave 16 ] --
+TDS:Upgrade(7)
+TDS:Upgrade(7)
+TDS:VoteSkip(16)
+
+-- [ Wave 17 ] --
+TDS:VoteSkip(17)
+TDS:Upgrade(6)
+
+-- [ Wave 18 ] --
+TDS:Upgrade(7)
+TDS:VoteSkip(18)
+TDS:VoteSkip(18)
+TDS:Sell(1)
+TDS:Sell(2)
+TDS:Sell(3)
+TDS:Sell(4)
+TDS:Sell(5)
+TDS:Sell(6)
+TDS:Sell(7)
+TDS:Sell(8)
+TDS:Sell(9)
+TDS:Sell(10)
+TDS:Sell(11)
+TDS:Sell(12)
+TDS:Place("Soldier", 2.5520992279052734, 262.99998474121094, 270.99102783203125)
+TDS:Upgrade(13)
+TDS:Ready()
+TDS:Place("Assassin", 2.7608184814453125, 262.99998474121094, 283.85125732421875)
+TDS:Place("Assassin", -2.554379940032959, 262.99998474121094, 283.5507507324219)
+TDS:VoteSkip(5)
+TDS:VoteSkip(6)
+TDS:VoteSkip(6)
