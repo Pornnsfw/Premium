c912ee7a9a34da7df37f0bd73aeb1769c5d542b7
AX <pornnsfwsafe@gmail.com>
2026-09-11 20:56:29 -0700

    Update Configs.lua

--- Helper/Configs.lua
+++ Helper/Configs.lua
@@ -174,7 +174,7 @@ return {
         SkillTree = {},
         Maps = {"Lay By", "Simplicity"},
         Scripts = {
-            ["Lay By"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
+            ["Lay By"] = "https://raw.githubusercontent.com/Pornnsfw/Premium/refs/heads/main/Fallback/MoltenLayby.lua",
             ["Simplicity"] = "https://raw.githubusercontent.com/AmonguszzZ/ModdedAether/refs/heads/main/Strats/Mode.lua",
         },
     },
